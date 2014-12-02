6298f29127023ba1d78907ea7e7dcd698a4135d7
block_given? <block_given@outlook.com>
2014-12-02 23:20:34 +0900

    translate by using tmx

--- src/helloworld.jsx
+++ src/helloworld.jsx
@@ -3,7 +3,74 @@ React.render(
     document.getElementById('example')
 );
 
-React.render(
-    <div>Hello world!</div>,
+var Source = React.createClass({
+    getInitialState: function() {
+	return {
+	    content: "Hello world! Hello, React!"
+	}
+    },
+
+    render: function() {
+	return (
+	    <pre>{this.state.content}</pre>
+	);
+    }
+});
+
+var source = React.render(
+    <Source lang="en" />,
     document.getElementById('source')
+);
+
+var Translation = React.createClass({
+    getInitialState: function() {
+	return {
+	    content: ""
+	}
+    },
+
+    componentDidMount: function() {
+	this.setState({
+	    content: source.state.content
+	});
+	$.get(this.props.tmx, function(result) {
+	    if (!this.isMounted()) {
+		return;
+	    }
+	    var tmxDoc = $.parseXML(result),
+	    $tmx = $(tmxDoc),
+	    translation = this,
+	    translatedContent = source.state.content;
+
+	    $tmx.find('tu').each(function() {
+		$source_tuv = $(this).find('tuv').filter(function() {
+		    return $(this).attr('xml:lang') == source.props.lang;
+		});
+		$translation_tuv = $(this).find('tuv').filter(function() {
+		    return $(this).attr('xml:lang') == translation.props.lang;
+		});
+		if ($source_tuv.length == 0 || $translation_tuv.length == 0) {
+		    return;
+		}
+		$source_tuv.find('seg').contents().each(function(){
+		    console.log(this.textContent);
+		    translatedContent = translatedContent.replace(new RegExp(this.textContent, 'g'), $translation_tuv.find('seg').text());
+		});
+		translation.setState({content: translatedContent});
+	    });
+	}.bind(this));
+    },
+
+    render: function() {
+	return (
+	    <pre>
+	      {this.state.content}
+	    </pre>
+	);
+    }
+});
+
+React.render(
+    <Translation source="source" tmx="https://blockgiven.github.io/ptarmigan/src/helloworld.tmx" lang="fr" />,
+    document.getElementById('translation')
 );
